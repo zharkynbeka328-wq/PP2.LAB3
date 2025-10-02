38126a1b81273918aeba87dcf7ce42bee2ea942e
Zharkynbek Akmaral <maralym@Zharkynbeks-Laptop.local>
2025-10-02 20:35:22 +0500

    +

---
+++ 2function5.py
@@ -0,0 +1,26 @@
+#Найти средний рейтинг фильмов категории
+
+movies = [
+    {"name": "Usual Suspects", "imdb": 7.0, "category": "Thriller"},
+    {"name": "Hitman", "imdb": 6.3, "category": "Action"},
+    {"name": "Dark Knight", "imdb": 9.0, "category": "Adventure"},
+    {"name": "The Help", "imdb": 8.0, "category": "Drama"},
+    {"name": "The Choice", "imdb": 6.2, "category": "Romance"},
+    {"name": "Colonia", "imdb": 7.4, "category": "Romance"},
+    {"name": "Love", "imdb": 6.0, "category": "Romance"},
+    {"name": "Bride Wars", "imdb": 5.4, "category": "Romance"},
+    {"name": "AlphaJet", "imdb": 3.2, "category": "War"},
+    {"name": "Ringing Crime", "imdb": 4.0, "category": "Crime"},
+    {"name": "Joking muck", "imdb": 7.2, "category": "Comedy"},
+    {"name": "What is the name", "imdb": 9.2, "category": "Suspense"},
+    {"name": "Detective", "imdb": 7.0, "category": "Suspense"},
+    {"name": "Exam", "imdb": 4.2, "category": "Thriller"},
+    {"name": "We Two", "imdb": 7.2, "category": "Romance"}
+]
+
+def average_by_category(movies, category):
+    cat_movies = [m for m in movies if m["category"].lower() == category.lower()]
+    return sum(m["imdb"] for m in cat_movies) / len(cat_movies) if cat_movies else 0
+
+print("Средний рейтинг Romance:", average_by_category(movies, "Romance"))
+print("Средний рейтинг Thriller:", average_by_category(movies, "Thriller"))
