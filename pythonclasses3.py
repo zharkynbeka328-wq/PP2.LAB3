eafccb8bcd224febda74317a4c43cb76936622ce
Zharkynbek Akmaral <maralym@Zharkynbeks-Laptop.local>
2025-10-02 19:55:32 +0500

    добавлен

---
+++ pythonclasses3.py
@@ -0,0 +1,15 @@
+#Rectangle от Shape
+class Shape:
+    def area(self):
+        return 0
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+rect = Rectangle(3, 9)
+print("Площадь прямоугольника:", rect.area())  
