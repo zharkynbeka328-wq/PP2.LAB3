3026956758996a5b60c7557562423409562c300d
Zharkynbek Akmaral <maralym@Zharkynbeks-Laptop.local>
2025-10-02 20:16:48 +0500

    +

---
+++ 1function7.py
@@ -0,0 +1,11 @@
+#has_33
+
+def has_33(nums):
+    for i in range(len(nums)-1):
+        if nums[i] == nums[i+1] == 3:
+            return True
+    return False
+
+print(has_33([1,3,3]))      # True
+print(has_33([1,3,1,3]))    # False
+print(has_33([3,1,3]))      # False
